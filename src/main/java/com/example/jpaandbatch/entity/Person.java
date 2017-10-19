3f81037ed353a6aefc4b031ab8b34a73358683a8
Arthur Carlsson <arthur@kiron.net>
2017-10-19 21:56:29 +0200

    Initial commit

---
+++ src/main/java/com/example/jpaandbatch/entity/Person.java
@@ -0,0 +1,25 @@
+package com.example.jpaandbatch.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private String name;
+
+    public Person() {
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
